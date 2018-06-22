c8d9942eb7a1bb1f1a927165a06169eb66126633
Brandon Clary <bclary@office.jlab.org>
2018-06-22 13:45:14 -0400

    changed example code

--- test_pid.groovy
+++ test_pid.groovy
@@ -16,13 +16,15 @@ System.out.println(" >> TEST ELECTRONPID ");
 int run_number = 4031;
 String in_file = INSERT FILE PATH HERE;
 
+//SET RUN PROPERTIES - VERY IMPORTANT
 RunPropertiesLoader run_properties = new RunPropertiesLoader(run_number,"DATA");
+
+//SET CUT VALUES - CRITICAL, IF NOT DONE THE CODE WILL CRASH ON PURPOSE
 CutLoader cut_loader = new CutLoader(run_number,"DATA","cut_nom");
 
-HipoDataSource reader = new HipoDataSource();
-
 ElectronPID find_el = new ElectronPID();
-find_el.initializeCuts();
+
+HipoDataSource reader = new HipoDataSource();
 
 reader.open(in_file);
 DataEvent nullevent;// = 0;
@@ -30,9 +32,15 @@ DataEvent nullevent;// = 0;
 while( reader.hasEvent() ){
 
        DataEvent event = reader.getNextEvent();
-       //HashMap<Boolean, Integer> m_el_result = find_el.getResult(event);
-       Particle final_electron = find_el.getParticle(event);
-       //System.out.println(">> " +  m_el_result );       
+       // CAN GET EITHER A MAP WITH INDEX AND TRUTH VALUE OF CANDIDATE 
+       // OR A PARTICLE AFTER FINDING THE GOOD ELECTRON CANDIDATE
+       
+       HashMap<Boolean, Integer> m_el_result = find_el.getResult(event);
+       //Particle final_electron = find_el.getParticle(event);
+       System.out.println(">> " +  m_el_result );       
+
+
+       // DO WHATEVER YOU WANT TO DO HERE     
 }
 
 
